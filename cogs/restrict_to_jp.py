d721dc730ee8aef69fea299b4fc0b116ce709b6b
friedrich-de <isimatranslations@gmail.com>
2021-12-14 14:32:33 +0100

    Unicode RE Strings.

--- cogs/restrict_to_jp.py
+++ cogs/restrict_to_jp.py
@@ -18,11 +18,29 @@ class LanguageDetect(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.active = False
+        emoji_pattern = re.compile("["
+                                   u"\U0001F600-\U0001F64F"  # emoticons
+                                   u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                                   u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                                   u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                                   u"\U00002702-\U000027B0"
+                                   u"\U00002702-\U000027B0"
+                                   u"\U000024C2-\U0001F251"
+                                   u"\U0001f926-\U0001f937"
+                                   u"\U00010000-\U0010ffff"
+                                   u"\u2640-\u2642"
+                                   u"\u2600-\u2B55"
+                                   u"\u200d"
+                                   u"\u23cf"
+                                   u"\u23e9"
+                                   u"\u231a"
+                                   u"\ufe0f"  # dingbats
+                                   u"\u3030"
+                                   "]+", flags=re.UNICODE)
+
         self.remove_res = [re.compile(r'<#\d+>'), re.compile(r'<a?:\w+:\d+>'), re.compile(r'<@!?\d+>'),
                            re.compile(r'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)'),
-                           re.compile(u'\U0001F600-\U0001F64F'), re.compile(u'\U0001F300-\U0001F5FF'),
-                           re.compile(u'\U0001F680-\U0001F6FF'), re.compile(u'\U0001F1E0-\U0001F1FF'),
-                           re.compile(u'\U00002600-\U000026FF')]
+                           emoji_pattern]
 
     async def format_message(self, message: discord.Message):
         allowed_messages = ["ｗｗｗ", "ｗｗ", "www", "ww", "orz"]
@@ -31,7 +49,7 @@ class LanguageDetect(commands.Cog):
 
         new_message = message.content.strip().replace("\n", "")
         for regexp in self.remove_res:
-            new_message = regexp.sub('', new_message)
+            new_message = regexp.sub(r'', new_message)
 
         for text in allowed_messages:
             new_message = new_message.replace(text, '')
