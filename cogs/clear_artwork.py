f7b62dfdee2f82d71fa21928fc77af96ad4237e8
friedrich-de <nuehoujuu90@gmail.com>
2022-02-17 21:56:36 +0100

    Delete 12 hours old.

--- cogs/clear_artwork.py
+++ cogs/clear_artwork.py
@@ -2,6 +2,8 @@ import asyncio
 import discord
 from discord.ext import commands
 from discord.ext import tasks
+from datetime import datetime
+from datetime import timedelta
 import json
 
 
@@ -22,7 +24,8 @@ class Deleter(commands.Cog):
     async def clear_channel(self):
         artwork_channel = discord.utils.get(self.myguild.channels, name="artwork")
         print("Attempting to purge artwork")
-        await artwork_channel.purge(limit=1000)
+        delete_limit = timedelta(hours=12)
+        await artwork_channel.purge(limit=1000, before=datetime.utcnow() - delete_limit)
         print("Purged artwork.")
 
 def setup(bot):
