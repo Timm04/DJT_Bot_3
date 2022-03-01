6846f99e5fcdfd9e8195fc49c660c615b2d70bf1
friedrich-de <nuehoujuu90@gmail.com>
2022-03-01 23:15:08 +0100

    Clear bad words.

--- cogs/clear_artwork.py
+++ cogs/clear_artwork.py
@@ -19,14 +19,45 @@ class Deleter(commands.Cog):
     async def on_ready(self):
         self.myguild = self.bot.get_guild(guild_id)
         self.clear_channel.start()
+        await asyncio.sleep(30)
+        self.clear_curse_words_loop.start()
 
-    @tasks.loop(minutes=600)
+    @tasks.loop(minutes=300)
     async def clear_channel(self):
         artwork_channel = discord.utils.get(self.myguild.channels, name="artwork")
         print("Attempting to purge artwork")
         delete_limit = timedelta(hours=12)
         await artwork_channel.purge(limit=1000, before=datetime.utcnow() - delete_limit)
         print("Purged artwork.")
+
+    @tasks.loop(minutes=30)
+    async def clear_curse_words_loop(self):
+        delete_limit = timedelta(hours=6)
+
+        def contains_curse_words(message: discord.Message):
+            if "nigger" in message.content.lower():
+                return True
+            elif "tranny" in message.content.lower():
+                return True
+            else:
+                return False
+
+        for channel in self.myguild.channels:
+            if isinstance(channel, discord.TextChannel):
+                print(f"Attempting to purge {channel.name}")
+                await asyncio.sleep(5)
+                purged_messages = await channel.purge(limit=1000, check=contains_curse_words,
+                                                      before=datetime.utcnow() - delete_limit)
+                if len(purged_messages) > 0:
+                    await channel.send(f"Deleted {len(purged_messages)} gamer messages.")
+
+        for thread in self.myguild.threads:
+            print(f"Attempting to purge {thread.name}")
+            await asyncio.sleep(5)
+            purged_messages = await thread.purge(limit=500, check=contains_curse_words,
+                                                  before=datetime.utcnow() - delete_limit)
+            if len(purged_messages) > 0:
+                await thread.send(f"Deleted {len(purged_messages)} gamer messages.")
 
 def setup(bot):
     bot.add_cog(Deleter(bot))
